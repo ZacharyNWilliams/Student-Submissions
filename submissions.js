f6a50252b46b350e336e7104a7f051ab6d6936db
ZacharyNWilliams <zacharynwilliams9@gmail.com>
2022-11-07 10:08:31 -0800

    updated work

--- submissions.js
+++ submissions.js
@@ -34,16 +34,15 @@ let submissions = [
 
     }
 
-]
+];
 
 //2
 function addSubmission(array, newName, newScore, newDate){
     console.log(array);
     let newSubmission = {
-        array:array,
-        newName:newName,
-        newScore:newScore,
-        newDate:newDate
+        name:newName,
+        score:newScore,
+        date:newDate
     };
 
     newSubmission.score >= 60? (newSubmission.passed = true): (newSubmission.passed = false);
@@ -56,22 +55,46 @@ addSubmission(submissions, "Jerry", 45, 2019-05-27);
 
 //3
 
-function deleteSubmissionsByIndex(array,Index){
-    
+function deleteSubmissionsByIndex(array,index){
+    array.splice(index,1)
+    console.log(array);
 }
+deleteSubmissionsByIndex(submissions,2);
 
 
 //4
-function deleteSubmissionsByName(array, name){
-    console.log(array.splice)
+function deleteSubmissionNyName(array, name){
+    let index = (element) => element.name === name;
+    array.splice(array.findIndex(index), 1);
+    console.log(submissions);
 }
-deleteSubmissionsByName(submissions,(0,1) )
+deleteSubmissionNyName(submissions, "Jack");
 
-//5
 
+// function deleteSubmissionsByName(array,name){
+//     let delSubmission = array.filter(subs => subs.name !== name);
+//     return delSubmission
+// }
+// console.log(deleteSubmissionsByName(submissions,"Jill"));
+
+// //5
+function editSubmission(array,index,score){
+    let newScore = array[index].score = score;
+    newScore >= 60? array[index].passed = true : array[index].passed = false;
+    return array;
+}
+console.log (editSubmission(submissions,2,50));
 //6
+function findSubmissionByName(array, name){
+    let find = array.find((array)=>{
+        return array.name === name;
 
-
+    });
+    console.log(find);
+}
+findSubmissionByName(submissions, "Joe")
 //7
 
 //8
+
+
